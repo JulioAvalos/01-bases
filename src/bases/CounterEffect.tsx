6e19d633d2cc74ea591dcab0f72d243048e38d32
JulioAvalos <julioavalos2@msn.com>
2022-01-06 19:51:38 -0600

    useRef + timelines

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { gsap } from 'gsap';
 
 const MAXIMUN_COUNT = 10;
 
 export const CounterEffect = () => {
+
   const [counter, setCounter] = useState(5);
+  const counterElement = useRef<HTMLHeadingElement>(null);
 
   const handleClick = () => {
     setCounter((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
@@ -18,16 +20,17 @@ export const CounterEffect = () => {
       'color:red; background-color:black;'
     );
 
-    gsap.to('.mi-objeto', { y: -10, duration: 0.2, ease: 'ease.out' }).then(() => {
-      gsap.to('.mi-objeto', { y: 0, duration: 1, ease: 'bounce.out' });
-    });
+    const tl = gsap.timeline();
+
+    tl.to(counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out' })
+      .to(counterElement.current, { y: 0, duration: 1, ease: 'bounce.out' });
 
   }, [counter]);
 
   return (
     <>
       <h1>CounterEffect: {counter}</h1>
-      <h2 className='mi-objeto'>{counter}</h2>
+      <h2 ref={counterElement}>{counter}</h2>
       <button onClick={handleClick}>+1</button>
     </>
   );
